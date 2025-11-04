a8b3ea56cbd8d8b2a57492463fa6ee2d9a0f2774
Craig Vanaman <craig.vanaman@aoe.com>
2025-11-04 10:17:27 +0100

    feat: volumes

---
+++ test/e2e/e2e_machine_volumes_test.go
@@ -0,0 +1,246 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"fmt"
+	"os/exec"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/aoepeople/machine-controller-manager-provider-stackit/test/utils"
+)
+
+var _ = Describe("MCM Provider STACKIT", func() {
+	Context("Machine volume configuration", func() {
+		It("should create a Machine with BootVolume configuration", func() {
+			secretName := generateResourceName("secret")
+			machineClassName := generateResourceName("machineclass")
+			machineName := generateResourceName("machine")
+
+			// Secret with minimal config
+			secretYAML := fmt.Sprintf(`
+apiVersion: v1
+kind: Secret
+metadata:
+  name: %s
+  namespace: %s
+type: Opaque
+stringData:
+  projectId: "12345678-1234-1234-1234-123456789012"
+  userData: |
+    #cloud-config
+    runcmd:
+      - echo "Base bootstrap from Secret"
+`, secretName, testNamespace)
+			createAndTrackResource("secret", secretName, testNamespace, secretYAML)
+
+			// MachineClass with BootVolume configuration
+			machineClassYAML := fmt.Sprintf(`
+apiVersion: machine.sapcloud.io/v1alpha1
+kind: MachineClass
+metadata:
+  name: %s
+  namespace: %s
+providerSpec:
+  machineType: "c1.2"
+  imageId: "550e8400-e29b-41d4-a716-446655440000"
+  bootVolume:
+    size: 100
+    performanceClass: "premium"
+  labels:
+    test: "bootvolume"
+secretRef:
+  name: %s
+  namespace: %s
+provider: STACKIT
+`, machineClassName, testNamespace, secretName, testNamespace)
+			createAndTrackResource("machineclass", machineClassName, testNamespace, machineClassYAML)
+
+			machineYAML := fmt.Sprintf(`
+apiVersion: machine.sapcloud.io/v1alpha1
+kind: Machine
+metadata:
+  name: %s
+  namespace: %s
+spec:
+  class:
+    kind: MachineClass
+    name: %s
+`, machineName, testNamespace, machineClassName)
+			createAndTrackResource("machine", machineName, testNamespace, machineYAML)
+
+			By("waiting for Machine to get a ProviderID")
+			Eventually(func() string {
+				cmd := exec.Command("kubectl", "get", "machine", machineName, "-n", testNamespace, "-o", "jsonpath={.spec.providerID}")
+				output, _ := utils.Run(cmd)
+				return output
+			}, MediumTimeout, StandardPoll).ShouldNot(BeEmpty(), "Machine should get a ProviderID")
+
+			By("verifying Machine has ProviderID in correct format")
+			cmd := exec.Command("kubectl", "get", "machine", machineName, "-n", testNamespace, "-o", "jsonpath={.spec.providerID}")
+			providerID, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(providerID).To(HavePrefix("stackit://"), "ProviderID should have stackit:// prefix")
+		})
+
+		It("should create a Machine with additional Volumes", func() {
+			secretName := generateResourceName("secret")
+			machineClassName := generateResourceName("machineclass")
+			machineName := generateResourceName("machine")
+
+			// Secret with minimal config
+			secretYAML := fmt.Sprintf(`
+apiVersion: v1
+kind: Secret
+metadata:
+  name: %s
+  namespace: %s
+type: Opaque
+stringData:
+  projectId: "12345678-1234-1234-1234-123456789012"
+  userData: |
+    #cloud-config
+    runcmd:
+      - echo "Base bootstrap from Secret"
+`, secretName, testNamespace)
+			createAndTrackResource("secret", secretName, testNamespace, secretYAML)
+
+			// MachineClass with additional volumes
+			machineClassYAML := fmt.Sprintf(`
+apiVersion: machine.sapcloud.io/v1alpha1
+kind: MachineClass
+metadata:
+  name: %s
+  namespace: %s
+providerSpec:
+  machineType: "c1.2"
+  imageId: "550e8400-e29b-41d4-a716-446655440000"
+  volumes:
+    - "550e8400-e29b-41d4-a716-446655440000"
+    - "660e8400-e29b-41d4-a716-446655440001"
+  labels:
+    test: "volumes"
+secretRef:
+  name: %s
+  namespace: %s
+provider: STACKIT
+`, machineClassName, testNamespace, secretName, testNamespace)
+			createAndTrackResource("machineclass", machineClassName, testNamespace, machineClassYAML)
+
+			machineYAML := fmt.Sprintf(`
+apiVersion: machine.sapcloud.io/v1alpha1
+kind: Machine
+metadata:
+  name: %s
+  namespace: %s
+spec:
+  class:
+    kind: MachineClass
+    name: %s
+`, machineName, testNamespace, machineClassName)
+			createAndTrackResource("machine", machineName, testNamespace, machineYAML)
+
+			By("waiting for Machine to get a ProviderID")
+			Eventually(func() string {
+				cmd := exec.Command("kubectl", "get", "machine", machineName, "-n", testNamespace, "-o", "jsonpath={.spec.providerID}")
+				output, _ := utils.Run(cmd)
+				return output
+			}, MediumTimeout, StandardPoll).ShouldNot(BeEmpty(), "Machine should get a ProviderID")
+
+			By("verifying Machine has ProviderID in correct format")
+			cmd := exec.Command("kubectl", "get", "machine", machineName, "-n", testNamespace, "-o", "jsonpath={.spec.providerID}")
+			providerID, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(providerID).To(HavePrefix("stackit://"), "ProviderID should have stackit:// prefix")
+		})
+
+		It("should create a Machine with both BootVolume and Volumes", func() {
+			secretName := generateResourceName("secret")
+			machineClassName := generateResourceName("machineclass")
+			machineName := generateResourceName("machine")
+
+			// Secret with minimal config
+			secretYAML := fmt.Sprintf(`
+apiVersion: v1
+kind: Secret
+metadata:
+  name: %s
+  namespace: %s
+type: Opaque
+stringData:
+  projectId: "12345678-1234-1234-1234-123456789012"
+  userData: |
+    #cloud-config
+    runcmd:
+      - echo "Base bootstrap from Secret"
+`, secretName, testNamespace)
+			createAndTrackResource("secret", secretName, testNamespace, secretYAML)
+
+			// MachineClass with both BootVolume and Volumes
+			machineClassYAML := fmt.Sprintf(`
+apiVersion: machine.sapcloud.io/v1alpha1
+kind: MachineClass
+metadata:
+  name: %s
+  namespace: %s
+providerSpec:
+  machineType: "c1.2"
+  imageId: "550e8400-e29b-41d4-a716-446655440000"
+  bootVolume:
+    size: 50
+    performanceClass: "standard"
+  volumes:
+    - "550e8400-e29b-41d4-a716-446655440000"
+  labels:
+    test: "bootvolume-and-volumes"
+secretRef:
+  name: %s
+  namespace: %s
+provider: STACKIT
+`, machineClassName, testNamespace, secretName, testNamespace)
+			createAndTrackResource("machineclass", machineClassName, testNamespace, machineClassYAML)
+
+			machineYAML := fmt.Sprintf(`
+apiVersion: machine.sapcloud.io/v1alpha1
+kind: Machine
+metadata:
+  name: %s
+  namespace: %s
+spec:
+  class:
+    kind: MachineClass
+    name: %s
+`, machineName, testNamespace, machineClassName)
+			createAndTrackResource("machine", machineName, testNamespace, machineYAML)
+
+			By("waiting for Machine to get a ProviderID")
+			Eventually(func() string {
+				cmd := exec.Command("kubectl", "get", "machine", machineName, "-n", testNamespace, "-o", "jsonpath={.spec.providerID}")
+				output, _ := utils.Run(cmd)
+				return output
+			}, MediumTimeout, StandardPoll).ShouldNot(BeEmpty(), "Machine should get a ProviderID")
+
+			By("verifying Machine has ProviderID in correct format")
+			cmd := exec.Command("kubectl", "get", "machine", machineName, "-n", testNamespace, "-o", "jsonpath={.spec.providerID}")
+			providerID, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(providerID).To(HavePrefix("stackit://"), "ProviderID should have stackit:// prefix")
+		})
+	})
+})
