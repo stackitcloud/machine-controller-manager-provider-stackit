6999aa0cd99fdea345cbfbd9195e7c78a14fd248
Aniruddha Basak <aniruddha.basak@stackit.cloud>
2026-09-07 12:09:07 +0200

    add tests for the create and delete machine

--- pkg/client/stackit.go
+++ pkg/client/stackit.go
@@ -25,7 +25,7 @@ type StackitClient interface {
 	ListServers(ctx context.Context, projectID, region string, labelSelector map[string]string) ([]*Server, error)
 	// GetNICsForServer retrieves a network interfaces for a given server
 	GetNICsForServer(ctx context.Context, projectID, region, serverID string) ([]*NIC, error)
-	// ListNics list all nics for a network
+	// ListNICs list all nics for a network
 	ListNICs(ctx context.Context, projectID, region, networkID string) ([]*NIC, error)
 	// DeleteNIC delete a given nic by ID
 	DeleteNIC(ctx context.Context, projectID, region, networkID, nicID string) error
